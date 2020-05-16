0b4ea8e1b0763446a1ca8be9c8c63b2d85e08b33
Elynejs <matthias.gomes03@gmail.com>
2020-05-16 17:44:13 +0200

    .

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import json, requests, time
 def fetch_id():
     with open('next_change_id.txt', 'r') as f:
         next_change_id = f.read()
-        print(next_change_id)
         return next_change_id
 
 
-def fetch_data(next_change_id):
-    r = requests.get(f'http://api.pathofexile.com/public-stash-tabs/?id={next_change_id}')
-    data = r.json()['next_change_id']
-    print(r.json())
-    with open(f'{next_change_id}.json', 'w', encoding='utf-8') as f:
-        json.dump(r.json(), f, ensure_ascii=False, indent=4)
+def fetch_data(id):
+    r = requests.get(f'http://api.pathofexile.com/public-stash-tabs/?id={id}')
+    next_id = r.json()['next_change_id']
+    with open(f'{id}.json', 'w', encoding='utf-8') as f:
+        json.dump(r.json(), f)
     with open('next_change_id.txt', 'w') as f:
-        f.write(data)
+        f.write(next_id)
+    return r.json()
 
+def compute_data(raw):
+    print('Number of fetched stashes : ' + len(raw['stashes']))
 
 next_change_id = fetch_id()
-fetch_data(next_change_id)
+raw = fetch_data(next_change_id)
+compute_data(raw)
 
